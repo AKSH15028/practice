e9b018fd138be9d5dabfe49f9a38d64c6be7c463
AKSH15028 <akshvashishtha3@gmail.com>
2026-06-28 15:09:45 +0530

    the main page login signup page and header page are created and the routing is done for all the pages.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Signup } from './signup/signup';
+import { Homepage } from './homepage/homepage';
+import { Header } from './header/header';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'signup', component: Signup },
+  {path: 'home', component: Homepage},
+  { path: 'header', component: Header }
+];
